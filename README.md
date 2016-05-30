f54eab8c47bb0b975996582f8407bde922a18719
wuzhenli <wuzhenli@kongfz.com>
2016-05-30 17:54:50 +0800

    修改README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # app_web_interactive
 手机app和网页界面交互，测试。
+利用UIWebView，加载孔夫子旧书网m站，
+    1）自动登录
--- app_web/app_web/ViewController.m
+++ app_web/app_web/ViewController.m
@@ -69,6 +69,7 @@
     
     return YES;
 }
+
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
 //    NSLog(@"%s",__func__);
 }
